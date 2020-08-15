a355472b3c0cd296d2ba6525c44b01034f0fe8e3
Jckpt <64931967+Jckpt@users.noreply.github.com>
2020-08-15 16:10:53 +0200

    added reason

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 var your_message = '/timeout'; // tutaj zmienic wiadomosc przed nickiem np. /ban /timeout itd.
 var how_much = '100'; //  tutaj zmienic czas jezeli chcesz dac /timeout (zostawic puste jak nie chcesz dac timeout tylko inna komende)
+var comment = 'sory ale śmierdzisz'; // komentarz dla osoby ktora dostanie bana lub timeout'a
 const tmi = require('tmi.js');
 const client = new tmi.Client({
   options: { debug: true },
@@ -22,6 +23,6 @@ client.on('message', async (channel, tags, message, self) => {
   let data = fs.readFileSync('paste_users.json');
   var parsed_json = await JSON.parse(data);
   for (var i = 0; i < parsed_json.length; i++) {
-    client.say(channel, `${your_message} ${parsed_json[i].nick} ${how_much}`);
+    client.say(channel, `${your_message} ${parsed_json[i].nick} ${how_much} ${comment}`);
   }
 });
